329f5dcbb276736139baa48812217fb9221cebb0
10Nikus <kijasnikodem@gmail.com>
2024-02-04 20:46:19 +0100

    Refactor page component to fetch feedback data dynamically

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,18 +1,38 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import DATA from "../data.json";
 import FeedbackListItem from "../components/FeedbackList/FeedbackListItem";
 import GoBackBtn from "../components/Header/GoBackBtn";
 import Link from "next/link";
 import Comment from "../components/FeedbackDetails/Comments";
 import AddComent from "../components/FeedbackDetails/AddComments";
+import { useEffect, useState } from "react";
 
-const ListData = DATA["productRequests"];
+interface FeedbackItem {
+  id: number;
+  // Add other properties as needed
+  comments: Comment[]; // Assuming comments is an array of Comment type
+}
 
 export default function Page() {
   const { id } = useParams();
-  const item = ListData.filter((item) => item.id === Number(id));
+  const [item, setData] = useState<FeedbackItem | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`api/posts/${id}`);
+        const result = await response.json();
+
+        setData(result.feedback);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
   return (
     <main className="bg-slate-100 px-36 py-20">
       <div className="flex justify-between m-4  w-full">
@@ -24,11 +44,11 @@ export default function Page() {
           Edit Feedback
         </Link>
       </div>
-      <FeedbackListItem feedback={item[0]} />
+      <FeedbackListItem feedback={item} />
       <div className="flex  flex-col  py-8 gap-5 bg-white rounded-lg  m-4 w-full px-12">
-        <h1>{item[0]?.comments?.length} Comments</h1>
-        {item[0]?.comments?.map((comment) => (
-          <Comment key={comment.id} comment={comment} />
+        <h1>{item?.comments?.length} Comments</h1>
+        {item?.comments?.map((comment, index) => (
+          <Comment key={`${id}${index}`} comment={comment} />
         ))}
       </div>
       <AddComent id={+id} />
