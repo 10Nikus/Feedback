a8b538d99cf5fe1ceed30373ca97606addea35d6
10Nikus <kijasnikodem@gmail.com>
2024-02-04 17:42:26 +0100

    Refactor API routes and add database connection***

---
+++ src/app/api/posts/[id]/route.ts
@@ -0,0 +1,42 @@
+import dbConnect from "@/app/lib/db";
+import { Feedback } from "@/app/models/feedback";
+import { NextResponse } from "next/server";
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+
+  dbConnect();
+  const jsonBody = await req.json();
+  const {
+    newStatus: status,
+    newTitle: title,
+    newDescription: description,
+  } = jsonBody;
+
+  await Feedback.findByIdAndUpdate(id, { status, title, description });
+  return NextResponse.json({ message: "Feedback updated" });
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  dbConnect();
+  await Feedback.findByIdAndDelete(id);
+  return NextResponse.json({ message: "Feedback deleted" });
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  dbConnect();
+  const feedback = await Feedback.findOne({ _id: id });
+
+  return NextResponse.json({ feedback });
+}
--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,12 +1,42 @@
+import dbConnect from "@/app/lib/db";
 import { Feedback } from "@/app/models/feedback";
-import mongoose from "mongoose";
+
+import { NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid";
+import { z } from "zod";
+
+export async function POST(req: Request) {
+  const jsonBody = await req.json();
+
+  const { title, description, category } = jsonBody;
+
+  const data = {
+    id: uuidv4(),
+    title,
+    description,
+    category,
+    upVotes: 0,
+    status: "suggestion",
+    comments: [],
+  };
+
+  const schema = z.object({
+    title: z.string().min(3).max(50),
+    description: z.string().min(4).max(250),
+    category: z.enum(["feature", "enhancement", "bug", "ui", "ux"]),
+  });
+
+  if (!schema.safeParse(jsonBody)) {
+    return NextResponse.json(schema.parse(jsonBody), { status: 400 });
+  }
+
+  dbConnect();
+  Feedback.create(data);
+  return NextResponse.json({ data });
+}
 
 export async function GET() {
-  const mongoUrl = process.env.MONGO_URL;
-  if (!mongoUrl) {
-    return Response.json({ error: "MONGO_URL is not defined" });
-  }
-  mongoose.connect(mongoUrl);
+  dbConnect();
   const feedback = await Feedback.find();
-  return Response.json({ feedback });
+  return NextResponse.json({ feedback });
 }
