4cbe856455aae6da430426248554938303cb754e
10Nikus <kijasnikodem@gmail.com>
2024-02-05 12:36:43 +0100

    Add useEffect hook to fetch data and update form fields

--- src/app/components/Form/UpdateForm.tsx
+++ src/app/components/Form/UpdateForm.tsx
@@ -1,20 +1,39 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FeedbuckButton from "../Header/FeedbackButton";
 import CancelBtn from "./CancelBtn";
-import DATA from "../../data.json";
+import { useParams } from "next/navigation";
 
-export default function Form({ id }: { id: number }) {
-  const ListData = DATA["productRequests"];
-
-  const item = ListData.find((item) => item.id === id);
+export default function Form() {
+  const { id } = useParams();
 
   const [data, setData] = useState({
-    title: item?.title,
-    category: item?.category,
-    description: item?.description,
+    title: "",
+    category: "",
+    description: "",
   });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/api/posts/65bfd5729e364313890d8cde`
+        );
+        const result = await response.json();
+        setData((prev) => ({
+          ...prev,
+          title: result.feedback.title,
+          category: result.feedback.category,
+          description: result.feedback.description,
+        }));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
 
   function handleChange(
     e: React.ChangeEvent<
@@ -26,6 +45,29 @@ export default function Form({ id }: { id: number }) {
       [e.target.name]: e.target.value,
     });
   }
+
+  // async function onSubmit(e: FormEvent) {
+  //   e.preventDefault();
+
+  //   if (!data.title || !data.description) return;
+
+  //   try {
+  //     const res = await fetch("api/posts", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(data),
+  //     });
+
+  //     if (!res.ok)
+  //       throw new Error("An error occurred while submitting the form");
+
+  //     router.push("/");
+  //   } catch (error) {
+  //     console.error("Error submitting form", error);
+  //   }
+  // }
 
   return (
     <form className="bg-white p-10  flex flex-col gap-8">
@@ -62,6 +104,7 @@ export default function Form({ id }: { id: number }) {
           Include any specific comments on what should be improved, added, etc.
         </p>
         <textarea
+          name="description"
           className="bg-slate-200 w-full px-1 py-2 rounded-md"
           value={data.description}
           onChange={handleChange}
