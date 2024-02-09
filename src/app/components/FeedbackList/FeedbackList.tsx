0b51ca8cadd3fdb11a332432066771d37c680d70
10Nikus <kijasnikodem@gmail.com>
2024-02-09 11:37:47 +0100

    Add dispatch and useEffect to update feedback number

--- src/app/components/FeedbackList/FeedbackList.tsx
+++ src/app/components/FeedbackList/FeedbackList.tsx
@@ -1,17 +1,19 @@
 "use client";
 
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import FeedbackListItem from "./FeedbackListItem";
 import { useEffect, useState } from "react";
 import useFetch from "@/app/hooks/UseFetch";
 import useFilter from "@/app/hooks/UseFilter";
 import UseSort from "@/app/hooks/UseSort";
 import { useRouter } from "next/navigation";
+import { setNumber } from "@/app/lib/features/slice/feedbuckNumberSlice";
 
 export const FeedbackList = () => {
   const { data } = useFetch(`api/posts/`);
   const [feedbacks, setFeedbacks] = useState<any>([]);
 
+  const dispatch = useDispatch();
   const router = useRouter();
   const sortData = UseSort();
   const filterData = useFilter();
@@ -20,13 +22,20 @@ export const FeedbackList = () => {
   const sort = useSelector((state: any) => state.sortSlice.status);
 
   useEffect(() => {
-    function makeData() {}
-    const filteredData = filterData(data, filter, "category");
-    const sortedData = sortData(filteredData, sort);
-    setFeedbacks(sortedData);
-    router.refresh();
+    function makeData() {
+      const filteredData = filterData(data, filter, "category");
+      const sortedData = sortData(filteredData, sort);
+      setFeedbacks(sortedData);
+      router.refresh();
+    }
     makeData();
   }, [data, filter, sort]);
+
+  useEffect(() => {
+    if (feedbacks) {
+      dispatch(setNumber(feedbacks.length));
+    }
+  }, [feedbacks]);
 
   return (
     <div className="flex flex-col align-middle w-full items-center min-h-svh">
