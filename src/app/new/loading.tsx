51b46394cb6929674870577b8a46499e572ba144
10Nikus <kijasnikodem@gmail.com>
2024-02-10 13:40:53 +0100

    Update loading component in edit and new pages

--- src/app/new/loading.tsx
+++ src/app/new/loading.tsx
@@ -1,9 +1,11 @@
-import { Skeleton } from "@mui/material";
+import { Skeleton, Stack } from "@mui/material";
 
 export default function Loading() {
   return (
-    <main className="bg-slate-100 flex flex-col items-center px-44 h-svh">
-      <Skeleton variant="rounded" animation="wave" height={626} />
+    <main className="bg-slate-100 flex justify-center  items-center px-56 h-svh pt-10">
+      <Stack spacing={2} className=" flex justify-center">
+        <Skeleton variant="rounded" animation="wave" height={626} width={587} />
+      </Stack>
     </main>
   );
 }
